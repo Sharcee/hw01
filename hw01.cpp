ff1774bb96f3e53ea9a7a0378a65a20f7d7bb65c
Simon Aizpurua <saa13b@my.fsu.edu>
2017-02-02 15:24:32 -0500

    Added print functionality to path, duplicate node tests, fixed small errors

--- hw01.cpp
+++ hw01.cpp
@@ -3,16 +3,24 @@
 #include <string>
 
 using namespace std;
+
 std::string path_field(std::string& x);
+std::vector<int> path_nodes(const string & path);
+void print_nodes(const std::vector<int> &v);
 
 int main() {
     std::string str = "";
     std::string path = "";
+    std::vector<int> nodes;
 
     while ( getline(cin, str) ) {
+        std::cout << "**** Begin Tests ****" << "\n";
         std::cout << str << "\n";       // Print test
         path = path_field(str);
         std::cout << "PATH_FIELD: " << path << "\n\n";
+        nodes = path_nodes(path);
+        print_nodes(nodes);
+        std::cout << "**** End of Tests ****" << "\n" << endl;
     }
 
     return 0;
@@ -38,7 +46,37 @@ std::string path_field(std::string& x) {
     return path;
 }
 
-// std::vector<int> name(/* arguments */) {
-//     /* code */
-//     return 0;
-// }
+std::vector<int> path_nodes(const string & path) {
+    std::vector<int> nodes;
+    std::string node = "";
+    std::string prev = "";
+
+    for (auto& c : path) {
+        if( c == ' ') {
+            if ( prev == node ) {
+                std::cout << "Duplicate Node Detected" << "\n";
+                node.clear();
+                continue;
+            }
+            std::cout << "Pushing NODE: " << node << "\n";
+            nodes.push_back(atoi(node.c_str()));
+            prev = node;
+            node.clear();
+            continue;
+        }
+        if ( c == '[') {
+            break;
+        }
+        node += c;
+    }
+
+    return nodes;
+}
+
+void print_nodes(const std::vector<int> &v) {
+    std::cout << "Path: ";
+    for (std::vector<int>::const_iterator i = v.begin(); i != v.end(); ++i) {
+        std::cout << *i << " ";
+    }
+    std::cout << endl;
+}
